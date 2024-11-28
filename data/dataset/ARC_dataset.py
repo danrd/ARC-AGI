757042fbe5902e60beffb1faabca8fe114357d4f
Daniil Radyush <daniil.radyush@gmail.com>
2024-11-29 01:17:39 +0300

    augmentation functionality

--- data/dataset/ARC_dataset.py
+++ data/dataset/ARC_dataset.py
@@ -51,29 +51,50 @@ class ARCDataset:
         else:
             raise ValueError('You need to specify splitting for the dataset with options: train or full')
     
-    def create_tasks(self):
+    def create_tasks(self, augmentation):
         """Create a list of tasks for current splitting setting."""
         tasks = []
+        aug_tasks = []
         for key in self.tasks_keys:
             train_inp, train_out, test_inp, test_out = self.task_to_lists(key)
             subtasks = []
+            aug_subtasks = [[] for _ in range(14)]
             n = len(train_inp)
             for i in range(n):
                 label = f'{key}_{i}'
                 subtask = ARCSubtask(label, train_inp[i], train_out[i])
                 subtasks.append(subtask)
+                if augmentation:
+                    train_inp_aug_grids = augment_grid(train_inp[i])
+                    train_out_aug_grids = augment_grid(train_out[i])
+                    for j in range(14):
+                        label = f'{key}_{i}_aug_{j}'
+                        subtask = ARCSubtask(label, train_inp_aug_grids[j], train_out_aug_grids[j])
+                        aug_subtasks[j].append(subtask)
             task = ARCTask(key, subtasks, test_inp, test_out)
-            tasks.append(task)
+            tasks.append(task) 
+            if augmentation:
+                test_inp_aug_grids = augment_grid(test_inp)
+                test_out_aug_grids = augment_grid(test_out)
+                for i in range(14):
+                    aug_key = f'{key}_aug_{i}'
+                    task = ARCTask(aug_key, aug_subtasks[i], test_inp_aug_grids[i], test_out_aug_grids[i])
+                    aug_tasks.append(task)
+        if augmentation:
+            tasks = tasks + aug_tasks
         return tasks
     
-def prepare_dataset(test_ratio:float=0.2, use_eval_set:bool=False, prompts_modifications={}):
+def prepare_dataset(test_ratio:float=0.2, use_eval_set:bool=False, augmentation=False, prompts_modifications={}):
     """Prepare dataset creating prompts for all tasks."""
+    ARC_dataset = ARCDataset(augmentation=augmentation)
     all_tasks = []
     if use_eval_set:
         n_examples = 800
     else: 
        n_examples = 400
-    ARC_tasks = ARCDataset().tasks[0:n_examples]
+    ARC_tasks = ARC_dataset.tasks[0:n_examples]
+    if augmentation:
+        ARC_tasks  += ARC_dataset.tasks[n_examples:]
     for _, task in enumerate(ARC_tasks):
         text = compose_prompt(task, BASE_PROMPT, prompts_modifications)
         task_dict = {'text':text, 'solution':repr(prepare_grid_for_prompt(task.test_subtask.train_out, task.test_subtask.train_out_shape, concise=False))}
@@ -83,3 +104,15 @@ def prepare_dataset(test_ratio:float=0.2, use_eval_set:bool=False, prompts_modif
     dataset = dataset.shuffle(seed=42)
     dataset = dataset.train_test_split(test_size=test_ratio)   
     return dataset
+
+def augment_grid(grid:np.array)->List[np.array]:
+    new_grids = []
+    new_grids.append(np.rot90(grid,k=1))
+    new_grids.append(np.rot90(grid,k=2))
+    new_grids.append(np.rot90(grid,k=3))
+    new_grids.append(np.fliplr(grid))
+    new_grids.append(np.flipud(grid))
+    for inc in range(1, 10):
+        new_grid = grid * 10
+        new_grids.append(((new_grid+inc)%10)/10)
+    return new_grids

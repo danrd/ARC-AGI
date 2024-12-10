9fa3cf5450d28901abf7bd8124b77f5ae9c42c62
Daniil Radyush <daniil.radyush@gmail.com>
2024-12-10 20:07:41 +0300

    utils update

--- utils/utils.py
+++ utils/utils.py
@@ -3,11 +3,18 @@ import os
 import random
 import numpy as np 
 import torch
+import pickle
 
 def load_json(file_path):
     """Load JSON data from a file."""
     with open(file_path, 'r') as file:
         data = json.load(file)
+    return data
+
+def load_pickle(file_path):
+    """Load pickle data from a file."""
+    with open(file_path, 'r') as file:
+        data = pickle.load(file)
     return data
 
 def seed_everything(seed=42):

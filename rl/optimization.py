06af209c8c045647507e65e05b3b3b5035848e73
Daniil Radyush <daniil.radyush@gmail.com>
2025-02-11 00:53:12 +0300

    rl optimization update

--- rl/optimization.py
+++ rl/optimization.py
@@ -1,7 +1,11 @@
 import optuna
+import wandb
+import os
+from copy import copy 
 from rl.ARC_task import ARCTask, ARCSubtask
-from rl.training import train_on_subtask
+from rl.training import train_on_subtask, train_on_task
 from data.configs.rl_configs import load_PPO_config
+from data.dataset.ARC_dataset import ARCDataset
 
 def optimize_ppo(trial):
     """ Learning hyperparamters we want to optimise."""
@@ -36,3 +40,39 @@ def optimize_agent(target, rl_config):
     except KeyboardInterrupt:
         print('Interrupted by keyboard.')
     return study.best_params, study.best_trial
+
+def hp_tuning(task_idx:int, subtasks_idxs:list, rl_config_params, model_config_params, base_rl_config, base_model_config):
+    wandb.login(key=os.environ["wandb_key"])
+    dataset = ARCDataset()
+    for param in rl_config_params.keys():
+        rl_config = copy(base_rl_config)
+        for value in rl_config_params[param]:
+            rl_config[param] = value
+            for model_param in model_config_params.keys():
+                model_config = copy(base_model_config) 
+                for model_value in model_config_params[model_param]:
+                    model_config[model_param] = model_value
+                    merged_config = copy(rl_config)
+                    merged_config.update(model_config)
+                    merged_config['task_idx'] = task_idx
+                    merged_config['subtasks_idxs'] = subtasks_idxs
+                    run = wandb.init(project="ARC_RL", name='PPO', config=merged_config, group=str(merged_config['task_idx']))
+                    if subtasks_idxs[0] == -1:
+                        task = dataset.tasks[task_idx]
+                        accs_for_subtasks, lens_for_subtasks, agent, train_metrics = train_on_task(task=task, rl_config=rl_config, PPO_config=model_config, 
+                                                                                                   verbose=True, plot_grid_pred=True)
+                        wandb.log({"accuracies": list(accs_for_subtasks.values()), "mean_ep_lens": list(lens_for_subtasks.values())})
+                        for metric, val in train_metrics.items():
+                            wandb.log({metric: val})
+                        del agent  
+                    else:
+                        task = dataset.tasks[task_idx]
+                        subtask = task.subtasks[subtasks_idxs[0]]
+                        acc, mean_len, agent, callback = train_on_subtask(subtask=subtask, rl_config=rl_config, PPO_config=model_config, 
+                                                                     verbose=True, plot_grid_pred=True, debug=False)
+                        expl_var = round(callback.explained_variances[-1], 3)
+                        wandb.log({"accuracies": [acc], "mean_ep_lens":[mean_len], "expl_vars": [expl_var]})
+                        del agent, callback
+                    print('All trials were finished')
+                    wandb.finish()
+    return

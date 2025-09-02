4128aaa71eb117ca04b5673fdf3ac82bf576fb81
Daniil Radyush <daniil.radyush@gmail.com>
2025-09-02 23:22:26 +0300

    implicit padding excluded

--- rl/ARC_task.py
+++ rl/ARC_task.py
@@ -5,15 +5,12 @@ from symbolic.utils import adjust_grid_shape, check_grid_values
 class ARCSubtask:
     def __init__(self, label:str, train_inp:np.array, train_out:np.array, validate:bool=False):
         self.label = label
-        self.train_inp = adjust_grid_shape(train_inp)
-        self.train_out = adjust_grid_shape(train_out)
+        self.train_inp = train_inp
+        self.train_out = train_out
         self.train_inp_shape = train_inp.shape
         self.train_out_shape = train_out.shape
         self.prev_grid_size = 0
-        self.max_int = 0
-        assert (self.train_inp.shape==(30,30) and self.train_out.shape==(30,30)), f"Grids shapes are not (30,30), instead: {self.train_inp.shape}, {self.train_out.shape}"
-        if validate:
-            assert (check_grid_values(self.train_inp)==True) and (check_grid_values(self.train_out)==True), f"Invalid grid values for subtask {self.label}\n Input grid: {self.train_inp}\n Output grid: {self.train_out}" 
+        self.max_int = 0    
         
 class ARCTask:
     """Class for storing information for a task."""

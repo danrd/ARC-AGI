a38e1cc2589c50470a818dcbcf0d8f1dbbb84801
Daniil Radyush <daniil.radyush@gmail.com>
2025-09-02 23:23:17 +0300

    shape validation excluded

--- rl/ARC_task.py
+++ rl/ARC_task.py
@@ -3,7 +3,7 @@ import numpy as np
 from symbolic.utils import adjust_grid_shape, check_grid_values
 
 class ARCSubtask:
-    def __init__(self, label:str, train_inp:np.array, train_out:np.array, validate:bool=False):
+    def __init__(self, label:str, train_inp:np.array, train_out:np.array):
         self.label = label
         self.train_inp = train_inp
         self.train_out = train_out
@@ -11,12 +11,12 @@ class ARCSubtask:
         self.train_out_shape = train_out.shape
         self.prev_grid_size = 0
         self.max_int = 0    
-        
+
 class ARCTask:
     """Class for storing information for a task."""
-    def __init__(self, label:str, subtasks:List[ARCSubtask], test_inp:np.array, test_out:np.array, validate:bool=False):
+    def __init__(self, label:str, subtasks:List[ARCSubtask], test_inp:np.array, test_out:np.array):
         self.label = label
         self.subtasks = subtasks
         self.test_inp = test_inp
         self.test_out = test_out
-        self.test_subtask =  ARCSubtask(f'{label}_test', self.test_inp, self.test_out, validate)
+        self.test_subtask =  ARCSubtask(f'{label}_test', self.test_inp, self.test_out)

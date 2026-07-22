2ca2d8fdf7c50b4ed63c02204ce9f7fcd36d9871
Daniil Radyush <daniil.radyush@gmail.com>
2026-07-23 00:14:08 +0300

    fix(rl): delete outdated func

--- rl/training.py
+++ rl/training.py
@@ -80,7 +80,7 @@ def train_on_subtask(subtask, rl_config:dict, PPO_config:dict=None, agent_init=N
     agent = create_agent(rl_config=rl_config, vec_env=vec_env, model_config=PPO_config, 
                      path_to_pretrained=path_to_pretrained, agent_init=agent_init)
     metrics_list = ['train/loss', 'train/value_loss', 'train/clip_fraction', 'train/approx_kl', 'train/explained_variance', 'rollout/ep_rew_mean']
-    logger = ARCLogger('/kaggle/working/arc-challenge/data', ["stdout", "csv"], metrics_list, load_PPO_config()['verbose'])
+    logger = ARCLogger('/data/logs/rl', ["stdout", "csv"], metrics_list, load_PPO_config()['verbose'])
     agent.set_logger(logger)
     agent.learn(rl_config['total_steps'], callback=callback)
     acc, mean_len, grid_pred = evaluate_ARC_policy(agent, vec_env, n_eval_episodes=rl_config['n_eval_episodes'])
@@ -114,30 +114,3 @@ def train_on_task(task, rl_config:dict, PPO_config:dict=None, agent_init=None, v
     train_metrics['expl_vars'] = list(expl_vars.values())
     print(f'Accuracies for task: {list(accs_for_subtasks.values())}, Mean episode lengths for task: {list(lens_for_subtasks.values())}')
     return accs_for_subtasks, lens_for_subtasks, agent, train_metrics
-
-def train_agent(subtask, rl_config:dict, PPO_config:dict=None, agent_init=None, 
-                     path_to_pretrained=None, verbose=False, plot_grid_pred=False, debug=False):
-    seed = rl_config['seed']
-    seed_everything(seed)
-    train_metrics = {}
-    accs_for_subtasks = {}
-    lens_for_subtasks = {}
-    expl_vars = {}
-    subtasks = task.subtasks
-    vec_env = create_vec_env(task.subtasks+[task.test_subtask], n_envs=rl_config['n_envs'], max_episode_len=rl_config['max_episode_len'], repr_level=rl_config['repr_level'],
-                             right_placement_reward=rl_config['right_placement_reward'],  action_penalty=rl_config['action_penalty'], 
-                             repetitive_actions_penalty=rl_config['repetitive_actions_penalty'], seed=seed, font_color=rl_config['font_color'], 
-                             padding=rl_config['padding'], input_pattern=rl_config['input_pattern'], milestones_rewards=rl_config['milestones_rewards'],
-                             pad_val=rl_config['pad_val'], reward_approach=rl_config['reward_approach'],
-                             feasible_actions=rl_config['feasible_actions'], observation_space_elements=rl_config['observation_space_elements'])
-    agent = create_agent(rl_config=rl_config, vec_env=vec_env, model_config=PPO_config, agent_init=agent_init)
-    metrics_list = ['train/loss', 'train/value_loss', 'train/clip_fraction', 'train/approx_kl', 'train/explained_variance', 'rollout/ep_rew_mean']
-    logger = ARCLogger('/kaggle/working/arc-challenge/data', ["stdout", "csv"], metrics_list, load_PPO_config()['verbose'])
-    agent.set_logger(logger)
-    agent.learn(rl_config['total_steps'], callback=callback)
-    acc, mean_len, grid_pred = evaluate_ARC_policy(agent, vec_env, n_eval_episodes=len(subtasks)+1)
-    if verbose:
-        print(f'Accuracy for {subtask.label}: {acc}. Mean episode length: {mean_len}')
-    if plot_grid_pred:
-        plot_grid(grid_pred)
-    return acc, mean_len, agent, callback, vec_env
